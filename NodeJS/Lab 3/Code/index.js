b620bf402416a9bc35ba9915bf697d6da72c94c2
“Mohamed <mohamed.kholy2011@gmail.com>
2026-02-12 15:06:25 +0200

    Update

--- NodeJS/Lab 3/Code/index.js
+++ NodeJS/Lab 3/Code/index.js
@@ -7,6 +7,9 @@ const inventoryRoutes = require("./routes/inventoryRoutes");
 const app = express();
 app.use(express.json());
 
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
 app.use(express.static(path.join(__dirname, "public")));
 
 homeRoutes(app);
--- NodeJS/Lab 3/Code/routes/homeRoutes.js
+++ NodeJS/Lab 3/Code/routes/homeRoutes.js
@@ -3,24 +3,6 @@ const { read } = require("../data/inventoryStore");
 module.exports = (app) => {
   app.get("/", (req, res) => {
     const products = read();
-    const html = `<!DOCTYPE html>
-<html>
-<head>
-  <title>Inventory</title>
-</head>
-<body>
-  <h2>Inventory List</h2>
-  <table border="1" cellpadding="5">
-    <tr>
-      <th>ID</th>
-      <th>Name</th>
-      <th>Quantity</th>
-      <th>Price</th>
-    </tr>
-    ${products.map((p) => `<tr><td>${p.id}</td><td>${p.name}</td><td>${p.quantity}</td><td>${p.price}</td></tr>`).join("")}
-  </table>
-</body>
-</html>`;
-    res.send(html);
+    res.render("index", { products });
   });
 };
