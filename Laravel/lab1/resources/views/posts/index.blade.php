40786dc9bd296cc13d32b9a7f39f0a99d7cadbea
Mohamed Sameh <mohamed.kholy2011@gmail.com>
2026-05-13 23:26:27 +0300

    Lab 1

---
+++ Laravel/lab1/resources/views/posts/index.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>ITI Blog - All Posts</h1>
+
+    <div style="margin-bottom: 2rem;">
+        <a href="/posts/create" class="btn btn-secondary">Create Post</a>
+    </div>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Posted By</th>
+                <th>Created At</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($posts as $post)
+                <tr>
+                    <td>{{ $post->title }}</td>
+                    <td>{{ $post->user->name }}</td>
+                    <td>{{ $post->created_at }}</td>
+                    <td>
+                        <div class="actions">
+                            <a href="/posts/{{ $post->id }}" class="btn btn-primary">View</a>
+                            <a href="/posts/{{ $post->id }}/edit" class="btn btn-secondary">Edit</a>
+                            <form class="inline-form" method="POST" action="/posts/{{ $post->id }}">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">Delete</button>
+                            </form>
+                        </div>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
