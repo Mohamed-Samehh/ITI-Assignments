96a2969bafec315116c74c1314f1d82337fd5f32
Mohamed Sameh <mohamed.kholy2011@gmail.com>
2026-06-01 14:01:48 +0300

    Update

---
+++ Odoo/Lab 1/hms/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Hospital Management System',
+    'version': '1.0',
+    'depends': ['base'],
+    'application': True,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/hms_patient_views.xml',
+    ],
+}
